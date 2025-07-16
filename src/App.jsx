f45b3a4f15800e77b669851341a1a716a7b59493
Pradeep-dhangar37 <163826083+Pradeep-dhangar37@users.noreply.github.com>
2025-07-16 20:06:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-// src/App.jsx
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Signup from './pages/Signup';
-// import Login from './pages/Login';
-// import Home from './pages/Home';
-// import Profile from './pages/Profile';
-// import ThemeProvider from './context/ThemeContext';
 import ThemeProvider from '../context/ThemeContext';
 import Signup from './pages/SignUp';
 import Login from './pages/Login';
